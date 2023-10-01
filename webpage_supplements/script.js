c396519b64beecc9679693453ec6c1c68b758939
williameclee <williameclee@gmail.com>
2023-10-01 02:59:01 -0700

    Changed page layout

--- webpage_supplements/script.js
+++ webpage_supplements/script.js
@@ -1,33 +1,3 @@
-// Include HTML
-// Modified after w3schools.com: https://www.w3schools.com/howto/howto_html_include.asp
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    // loop through a collection of all HTML elements:
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-    // search for elements with a certain attribute:
-      file = elmnt.getAttribute("include-html");
-      if (file) {
-        // make an HTTP request using the attribute value as the file name:
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            // remove the attribute, and call this function once more:
-            elmnt.removeAttribute("include-html");
-            includeHTML();
-          }
-        }      
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        // exit the function:
-        return;
-      }
-    }
-};
-
 // Accordion
 // Modified after w3schools.com: https://www.w3schools.com/howto/howto_js_accordion.asp
 var acc = document.getElementsByClassName("accordion");
@@ -36,14 +6,14 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         // Toggle between adding and removing the "active" class, to highlight the button that controls the panel
-        this.classList.toggle("accordion-disactive");
+        this.classList.toggle("accordion-active");
 
         // Toggle between hiding and showing the active panel
         var panel = this.nextElementSibling;
-        if (panel.style.display === "none") {
-        panel.style.display = "block";
-        } else {
+        if (panel.style.display === "block") {
         panel.style.display = "none";
+        } else {
+        panel.style.display = "block";
         }
     });
 }
